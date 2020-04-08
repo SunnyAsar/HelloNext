3a49a007a44135a6e53c394a12bcb7eb4e595b1e
Sunny Asar <ring2rings93@gmail.com>
2020-04-08 23:27:26 +0100

    dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+const PostLink = (props) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+)
+
 const index = () => {
   return (
     <div>
       <Layout>
-        <p>Hello Next.js</p>
+        <h1>My Blog</h1>
+        <ul>
+          <PostLink id="Hello Next.js" />
+          <PostLink id="Learn Next.js is awesome" />
+          <PostLink id="Deploy apps with Zeit" />
+        </ul>
       </Layout>
     </div>
   )
